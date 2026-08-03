0f26b1eee3f719b5038a4ee6ac593516105f6683
VALE10012 <echeverridjsv@gmail.com>
2026-08-03 09:13:25 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+function cargarpagina(pagina) {
+    fetch('pages/' + pagina)
+    .then(response => {
+        if(!response.ok) {
+            throw new Error ('Error al cargar la pagina');
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('contenido').innerHTML = data;
+        localStorage.setItem("paginaActual", pagina);
+    })
+    .catch(error => {
+        document.getElementById('contenido').innerHTML = 
+         "<p>Error cargando el contenido</p>";
+        console.error(error);
+    });
+}
+document.addEventListener("DOMContentLoaded", () => {
+   
+
+    const loginScreen = document.getElementById("loginScreen");
+    const appContent = document.getElementById("appContent");
+
+    const nombreGuardado = localStorage.getItem("nombreUsuario");
+
+    if (nombreGuardado) {
+
+        loginScreen.style.display = "none";
+        appContent.style.display = "block";
+
+    }
+
+    
+document.getElementById("formLogin").addEventListener("submit", function(e) {
+    // alert("bienvenido a mi sitio web");
+
+    e.preventDefault();
+
+    const nombre = document.getElementById("inputNombre").value.trim();
+
+    const regex = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+
+    if(nombre === "" || !regex.test(nombre)) {
+
+        alert("Ingrese un nombre válido");
+        const alertContainer = document.getElementById("alertContainer");
+
+        alertContainer.innerHTML = `
+         <div class="alert alert-danger alert-dismissible fade show" role="alert">
+           Ingrese un nombre válido
+           <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+         </div>
+        `;
+
+        setTimeout(() => {
+            alertContainer.innerHTML = "";
+        }, 3000);
+
+    }else{
+
+        document.getElementById("nombre").innerHTML = 
+        "Hola, " + nombre;
+
+        localStorage.setItem("nombreUsuario", nombre);
+
+        document.getElementById("loginScreen").style.display = "none";
+
+        document.getElementById("appContent").style.display = "block";
+    }
+
+});
+
+
+});
+
